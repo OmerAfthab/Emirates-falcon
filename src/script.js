5590ddeeba00aa296d685c5e1af9e01354972175
Inayat Yousuf <inayatyousuf77@gmail.com>
2022-10-10 13:59:17 +0530

    added footer and header in the seperate files and add them in all pages using Js

--- src/script.js
+++ src/script.js
@@ -12,38 +12,38 @@
  * navbar variables
  */
 
-const navOpenBtn = document.querySelector("[data-menu-open-btn]");
-const navCloseBtn = document.querySelector("[data-menu-close-btn]");
-const navbar = document.querySelector("[data-navbar]");
-const overlay = document.querySelector("[data-overlay]");
-
-const navElemArr = [navOpenBtn, navCloseBtn, overlay];
-
-for (let i = 0; i < navElemArr.length; i++) {
-
-  navElemArr[i].addEventListener("click", function () {
-
-    navbar.classList.toggle("active");
-    overlay.classList.toggle("active");
-    document.body.classList.toggle("active");
-
-  });
-
-}
 
 
+// const navOpenBtn = document.querySelector("[data-menu-open-btn]");
+// const navCloseBtn = document.querySelector("[data-menu-close-btn]");
+// const navbar = document.querySelector("[data-navbar]");
+// const overlay = document.querySelector("[data-overlay]");
 
-/**
- * header sticky
- */
+// const navElemArr = [navOpenBtn, navCloseBtn, overlay];
 
-const header = document.querySelector("[data-header]");
+// for (let i = 0; i < navElemArr.length; i++) {
 
-window.addEventListener("scroll", function () {
+//   navElemArr[i].addEventListener("click", function () {
 
-  window.scrollY >= 10 ? header.classList.add("active") : header.classList.remove("active");
+//     navbar.classList.toggle("active");
+//     overlay.classList.toggle("active");
+//     document.body.classList.toggle("active");
 
-});
+//   });
+
+// }
+
+
+
+
+
+// const header = document.querySelector("[data-header]");
+
+// window.addEventListener("scroll", function () {
+
+//   window.scrollY >= 10 ? header.classList.add("active") : header.classList.remove("active");
+
+// });
 
 
 
